fd7c1f32330c0447424d72105f18f69eeb5833c9
robertwim <45790250+robertwim@users.noreply.github.com>
2025-12-28 14:56:46 +0100

    2 x php

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+$host = "database-5019015271.webspace-host.com";
+$user = "dbu1065673";
+$pass = "Robertwim1947!";
+$db   = "dbs14968921";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Database-verbinding mislukt: " . $conn->connect_error);
+}
+
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+    $conn->query("DELETE FROM guestbook WHERE id = $id");
+}
+
+header("Location: index.php");
+exit;
+?>
